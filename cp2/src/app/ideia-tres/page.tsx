1cc067206a86cb1a0c97e6f367db56fa858a7567
rodrig1509 <digosmantovanello@gmail.com>
2024-09-30 19:44:25 -0300

    Conteúdo ideia 2.

--- cp2/src/app/ideia-tres/page.tsx
+++ cp2/src/app/ideia-tres/page.tsx
@@ -4,16 +4,8 @@ export default function page() {
     return (
       <div>
       <Cabecalho/>
-      
-      
-      
-      
-      
-      
-      
-      
-      
-
+      <h1>Interpretação Astronômica dos Mitos</h1>
+      <p>Velikovsky reinterpretou muitos mitos e textos antigos (como a Bíblia, mitologia grega e textos mesopotâmicos) como registros de eventos astronômicos reais. Ele sugeriu que muitas lendas de deuses lutando nos céus, dragões e serpentes de fogo são, na verdade, relatos simbólicos de interações planetárias, como o que ele acreditava ser o "nascimento" de Vênus e suas passagens próximas da Terra.</p>
       </div>
       
       
