0a117c6a76594b934753b2a5356290e8ff4e9bdc
rodrig1509 <digosmantovanello@gmail.com>
2024-09-30 19:45:30 -0300

    Conteúdo ideia 3.

--- cp2/src/app/ideia-um/page.tsx
+++ cp2/src/app/ideia-um/page.tsx
@@ -4,7 +4,9 @@ export default function page() {
     return (
       <div>
       <Cabecalho/>
-      
+      <h1>Rejeição do Uniformitarismo Científico</h1>
+      <p>Ele desafiou a visão científica dominante de seu tempo, conhecida como uniformitarismo, que sustenta que os processos geológicos e astronômicos ocorrem de maneira lenta e constante ao longo do tempo. Em vez disso, Velikovsky argumentou que mudanças dramáticas e repentinas moldaram o planeta em tempos históricos. Segundo ele, esses eventos catastróficos teriam afetado o clima, a geologia e até a órbita da Terra, o que explicaria certas anomalias na cronologia histórica.
+      Essas ideias foram amplamente rejeitadas pela comunidade científica, mas Velikovsky teve impacto duradouro em debates sobre catastrofismo e o papel de eventos astronômicos na história da Terra.</p>
       </div>
       
       
