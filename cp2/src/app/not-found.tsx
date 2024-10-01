dbb82f5dae7c6988bbdc4f75c1777ea9c377b16a
tcguus <gugucram15@gmail.com>
2024-09-30 22:36:15 -0300

    Commit final, trabalho finalizado.

--- cp2/src/app/not-found.tsx
+++ cp2/src/app/not-found.tsx
@@ -1,9 +1,15 @@
 import Cabecalho from '@/components/Cabecalho/Cabecalho';
 import { FC } from 'react';
+import Image from 'next/image'
  
 const NotFound: FC = () => {
   return (
-<div style={{ textAlign: 'center', marginTop: '50px' }}>
+<div className='flex flex-col justify-center items-center h-full w-full'>
+<Image src="/images/404.png"
+      alt="Erro 404"
+        width={400}
+        height={200}
+        />
 <h1>404 - Página não encontrada!</h1>
 <p>Oops! A página que você está olhando não existe.</p>
 <Cabecalho/>
